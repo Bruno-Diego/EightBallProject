f80ba5cd813b46f6c4b69547b8494b1766cefe35
Bruno-Diego <bruno.diego.x@hotmail.com>
2022-02-15 18:22:06 +0000

    first commit

---
+++ scrypt.js
@@ -0,0 +1,48 @@
+function tell() {
+  const username = document.getElementById("username").value;
+  let userQuestion = document.getElementById("question").value;
+  const randomNumber = Math.floor(Math.random() * 8);
+  let eightBall = "";
+  switch (randomNumber) {
+    case 0:
+      eightBall = "It is certain!";
+      break;
+    case 1:
+      eightBall = "It is decidedly so!";
+      break;
+    case 2:
+      eightBall = "Reply hazy try again...";
+      break;
+    case 3:
+      eightBall = "Cannot predict now :(";
+      break;
+    case 4:
+      eightBall = "Do not count on it...";
+      break;
+    case 5:
+      eightBall = "My sources say no!";
+      break;
+    case 6:
+      eightBall = "Outlook not so good!";
+      break;
+    case 7:
+      eightBall = "Signs point to yes!";
+      break;
+    default:
+      eightBall = "Error!";
+      break;
+  }
+  var newText = document.createElement("h2");
+  var result = document.createTextNode(
+    "Hi! " +
+      username +
+      ", you asked: " +
+      userQuestion +
+      " And here is your answare: " +
+      eightBall
+  );
+  newText.appendChild(result);
+  var currentDiv = document.getElementById("div-result");
+  currentDiv.appendChild(newText);
+  //console.log(eightBall);
+}
